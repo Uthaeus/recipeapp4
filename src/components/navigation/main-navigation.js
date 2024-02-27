a0e264029905cbd2207dd7cba5d8d663c6e69a13
Uthaeus <romanlavery@gmail.com>
2024-02-27 10:37:59 -0700

    added user context and logout to main nav

--- src/components/navigation/main-navigation.js
+++ src/components/navigation/main-navigation.js
@@ -1,6 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../../store/userContext";
 
 function MainNavigation() {
+    const { user, logout } = useContext(UserContext);
+
     return (
         <nav className="main-navigation">
             <div className="main-navigation-left">
@@ -9,8 +13,12 @@ function MainNavigation() {
             </div>
 
             <div className="main-navigation-right">
-                <NavLink to="/login" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Login</NavLink>
-                <NavLink to="/signup" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Signup</NavLink>
+                { user ? <Link to='/' onClick={logout} className="main-nav-link">Logout</Link> :
+                <>
+                    <NavLink to="/login" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Login</NavLink>
+                    <NavLink to="/signup" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Signup</NavLink>
+                </>
+                }
             </div>
         </nav>
     );
