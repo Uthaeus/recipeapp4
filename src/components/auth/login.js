d4e9f5e391621a58787b88da79b5b4c315d98c51
Uthaeus <romanlavery@gmail.com>
2024-02-26 19:39:20 -0700

    added styling to login and signup, outputting info on submit

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -13,13 +13,13 @@ export default function Login() {
             <p className="auth-title">login</p>
 
             <form onSubmit={handleSubmit(submitHandler)} className="auth-form">
-                <div className="form-group">
+                <div className="form-group mb-3">
                     <label htmlFor="username">Username</label>
                     <input type="text" id="username" className="form-control" {...register("username", { required: true })} />
                     {errors.username && <p className="auth-warning-text">Username is required</p>}
                 </div>
 
-                <div className="form-group">
+                <div className="form-group mb-4">
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" className="form-control" {...register("password", { required: true })} />
                     {errors.password && <p className="auth-warning-text">Password is required</p>}
@@ -28,7 +28,7 @@ export default function Login() {
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
 
-            <Link to="/signup">Signup</Link>
+            <Link to="/signup" className="auth-link">Signup</Link>
         </div>
     );
 }
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -13,27 +13,27 @@ export default function Signup() {
             <p className="auth-title">signup</p>
             
             <form onSubmit={handleSubmit(submitHandler)} className="auth-form">
-                <div className="form-group">
-                    <label htmlFor="username">Username *</label>
+                <div className="form-group mb-3">
+                    <label htmlFor="username">Username*</label>
                     <input type="text" id="username" className="form-control" {...register("username", { required: true })} />
                     {errors.username && <p className="auth-warning-text">Username is required</p>}
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="password">Password *</label>
+                <div className="form-group mb-3">
+                    <label htmlFor="password">Password*</label>
                     <input type="password" id="password" className="form-control" {...register("password", { required: true })} />
                     {errors.password && <p className="auth-warning-text">Password is required</p>}
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="passwordConfirm">Confirm Password *</label>
+                <div className="form-group mb-4">
+                    <label htmlFor="passwordConfirm">Confirm Password*</label>
                     <input type="password" id="passwordConfirm" className="form-control" {...register("passwordConfirm", { required: true })} />
                     {errors.passwordConfirm && <p className="auth-warning-text">Confirm Password is required</p>}
                 </div>
 
                 <button type="submit" className="btn btn-primary">Signup</button>
             </form>
-            <Link to="/login">Login</Link>
+            <Link to="/login" className="auth-link">Login</Link>
         </div>
     );
 }
