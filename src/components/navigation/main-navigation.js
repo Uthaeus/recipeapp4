452687e5ad410f1810b80f811bb15c446a9040d7
Uthaeus <romanlavery@gmail.com>
2024-02-26 18:53:07 -0700

    created main nav component, added links, begain initial styling

---
+++ src/components/navigation/main-navigation.js
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+
+function MainNavigation() {
+    return (
+        <nav className="main-navigation">
+            <div className="main-navigation-left">
+                <NavLink to="/home" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Home</NavLink>
+                <NavLink to="/contact" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Contact</NavLink>
+            </div>
+
+            <div className="main-navigation-right">
+                <NavLink to="/login" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Login</NavLink>
+                <NavLink to="/signup" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Signup</NavLink>
+            </div>
+        </nav>
+    );
+}
+
+export default MainNavigation;
--- src/components/root-layout.js
+++ src/components/root-layout.js
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router";
 
+import MainNavigation from "./navigation/main-navigation";
+
 export default function RootLayout() {
     return (
         <>
+            <MainNavigation />
             <Outlet />
         </>
     );
