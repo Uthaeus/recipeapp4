8809a9da8b58d595f62c9ee8048f25c0b93d0c63
Uthaeus <romanlavery@gmail.com>
2024-02-28 10:45:34 -0700

    initialized firebase, created env for config info

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,38 +1,17 @@
+import { useContext } from "react";
 
 import RecipeItem from "../components/home/recipe-item";
 import Sidebar from "../components/home/sidebar";
 
+import { RecipesContext } from "../store/recipesContext";
+
 function Home() {
-    const tempRecipes = [
-        {
-            id: 1,
-            title: 'Recipe 1',
-            description: 'Recipe 1 description',
-            ingredients: ['ingredient 1', 'ingredient 2'],
-            instructions: 'instructions 1',
-            time: '10 min',
-        },
-        {
-            id: 2,
-            title: 'Recipe 2',
-            description: 'Recipe 2 description',
-            ingredients: ['ingredient 1', 'ingredient 2'],
-            instructions: 'instructions 2',
-            time: '15 min',
-        },
-        {
-            id: 3,
-            title: 'Recipe 3',
-            description: 'Recipe 3 description',
-            ingredients: ['ingredient 1', 'ingredient 2'],
-            instructions: 'instructions 3',
-            time: '20 min',
-        },
-    ]
+    const { recipes } = useContext(RecipesContext);
+
     return (
         <div className="home">
             <div className="home-left">
-                {tempRecipes.map((recipe) => (
+                {recipes.map((recipe) => (
                     <RecipeItem key={recipe.id} recipe={recipe} />
                 ))}
             </div>
---
+++ src/store/recipesContext.js
@@ -0,0 +1,49 @@
+import { useState, useEffect, createContext } from "react";
+import { collection } from "firebase/firestore";
+
+import { db } from "../firebase-config";
+
+export const RecipesContext = createContext({
+    recipes: [],
+    setRecipes: () => {},
+    addRecipe: () => {},
+    deleteRecipe: () => {},
+});
+
+
+export default function RecipesContextProvider({ children }) {
+    const [recipes, setRecipes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const recipesCollectionRef = collection(db, "recipes");
+
+    useEffect(() => {
+        const getRecipes = async () => {
+            const data = await getDocs(recipesCollectionRef);
+            setRecipes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            setIsLoading(false);
+        };
+        getRecipes();
+    }, []);
+
+    const addRecipe = (recipe) => {
+        setRecipes([...recipes, recipe]);
+    };
+
+    const deleteRecipe = (id) => {
+        setRecipes(recipes.filter((recipe) => recipe.id !== id));
+    };
+
+    const value = {
+        recipes,
+        setRecipes,
+        addRecipe,
+        deleteRecipe,
+    };
+    
+    return (
+        <RecipesContext.Provider value={value}>
+            {!isLoading && children}
+        </RecipesContext.Provider>
+    );
+}
