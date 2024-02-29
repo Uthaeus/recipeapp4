456f7bb0712c0facdba1384faeab29cd327abcb4
Uthaeus <romanlavery@gmail.com>
2024-02-28 18:28:02 -0700

    imported temp default user image, using in main nav

--- src/components/navigation/main-navigation.js
+++ src/components/navigation/main-navigation.js
@@ -3,6 +3,8 @@ import { useContext } from "react";
 
 import { UserContext } from "../../store/userContext";
 
+import image from '../../assets/images/49er-thumb.png';
+
 function MainNavigation() {
     const { user, isLoggedIn, logout } = useContext(UserContext);
 
@@ -15,7 +17,7 @@ function MainNavigation() {
 
             <div className="main-navigation-right">
                 { isLoggedIn ? <>
-                    <Link to='/profile/edit'><img src={user.photoURL ? user.photoURL : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'} className="nav-profile-image" width={40} /></Link>
+                    <Link to='/profile/edit'><img src={user.photoURL ? user.photoURL : image} className="nav-profile-image" width={40} /></Link>
                     <p className="main-nav-link">Hello, {user.displayName ? user.displayName : user.email}</p>
                     <NavLink to="/recipe/new" className={({ isActive }) => isActive ? 'main-nav-link active' : 'main-nav-link'}>Create New Recipe</NavLink>
                     <Link to='/' onClick={logout} className="main-nav-link">Logout</Link>
